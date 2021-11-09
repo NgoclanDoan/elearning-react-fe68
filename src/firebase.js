969df358ae10d09992257914850c840bfa556676
Doanthingoclan <dtnlanat13m@gmail.com>
2021-11-10 02:06:08 +0700

    setting Firebase to deploy app

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyBDtjaj-ocrKUcC1EXBgtgP1yLIBxXN_Ok',
+	authDomain: 'elearning-react-6c299.firebaseapp.com',
+	projectId: 'elearning-react-6c299',
+	storageBucket: 'elearning-react-6c299.appspot.com',
+	messagingSenderId: '334641926015',
+	appId: '1:334641926015:web:de4af6d7373becdca4544a',
+};
